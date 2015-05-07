30622e5f0a05b55373447b3bb05ab958ce96bb9b
agus <agus@kurio.co.id>
2015-05-07 23:48:22 +0700

    callback after user private

--- application/views/layout/error_api_v.php
+++ application/views/layout/error_api_v.php
@@ -1,8 +1,8 @@
 <div style="height:50px"></div>
 <div class="center">
-    <h3><?php echo $error; ?></h3>
+    <h3><?php echo $error ?></h3>
     <br>
-    <a class="btn btn-primary" href="<?php echo isset($link)?$link:base_url(); ?>" >
+    <button class="btn btn-primary" onclick="goBack(this)" id="<?php echo isset($link)?$link:""; ?>" >
         <?php echo isset($back_text)?$back_text:"Back"; ?>
-    </a>
+    </button>
 </div>
